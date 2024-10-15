b5ba078ca9c1ae93fa2e516b8f9c477b6be0340e
Elad Levi <58475124+JakePeralta7@users.noreply.github.com>
2024-10-15 17:37:46 +0300

    Update reg_scout.py

--- reg_scout.py
+++ reg_scout.py
@@ -1,23 +1,34 @@
+
+# Externals Imports
 from regipy.registry import RegistryHive
-from regipy.utils import convert_wintime
 
+# Internals Imports
 from hives.system import SystemHive
-
-
-def handle_hive(reg_hive):
-    print("Software Hive Handled!")
-    print(reg_hive.get_key(r"\Microsoft\Windows NT\CurrentVersion\Winlogon").get_value("Shell"))
+from hives.software import SoftwareHive
+from hives.security import SecurityHive
 
 
 def main():
     reg_hive = RegistryHive(r"D:\Tools\KAPE\output\C\Windows\System32\config\SYSTEM")
     
     match reg_hive.hive_type:
+
+        # Hanldes the SYSTEM hive (located in the live registry in 'HKEY_LOCAL_MACHINE\SYSTEM\')
         case "system":
             system_hive = SystemHive(reg_hive)
-            print(system_hive.current_control_set)
+            computer_name = system_hive.get_computer_name()
+            print(computer_name)
+
+            last_shutdown_time = system_hive.get_last_shutdown()
+            print(last_shutdown_time)
+
+        # Hanldes the SOFTWARE hive (located in the live registry in 'HKEY_LOCAL_MACHINE\SOFTWARE\')
         case "software":
-            software.handle_hive(reg_hive)
+            software_hive = SoftwareHive(reg_hive)
+            print(software_hive.get_winlogon_shell())
+
+        case "security":
+            security_hive = SecurityHive(reg_hive)
 
 if __name__ == "__main__":
     main()
