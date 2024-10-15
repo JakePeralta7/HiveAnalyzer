38f9dabccd75f68daed8a3c2c3229aa7005d8285
Elad Levi <58475124+JakePeralta7@users.noreply.github.com>
2024-10-15 17:57:11 +0300

    Update software.py

--- hives/software.py
+++ hives/software.py
@@ -1,6 +1,9 @@
-class SoftwareHive():
+from hive import Hive
+
+
+class SoftwareHive(Hive):
     def __init__(self, reg_hive):
-        self.reg_hive = reg_hive
+        super().__init__(reg_hive)
 
     def get_winlogon_shell(self):
         return self.reg_hive.get_key(r"\Microsoft\Windows NT\CurrentVersion\Winlogon").get_value("Shell")
