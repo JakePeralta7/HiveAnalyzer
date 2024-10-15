f1fe9099f152ae01ac606a01f67497601bf5dac3
Elad Levi <58475124+JakePeralta7@users.noreply.github.com>
2024-10-15 17:39:40 +0300

    Update reg_scout.py

--- reg_scout.py
+++ reg_scout.py
@@ -3,15 +3,24 @@
 from regipy.registry import RegistryHive
 
 # Internals Imports
-from hives.system import SystemHive
-from hives.software import SoftwareHive
 from hives.security import SecurityHive
+from hives.software import SoftwareHive
+from hives.system import SystemHive
 
 
 def main():
     reg_hive = RegistryHive(r"D:\Tools\KAPE\output\C\Windows\System32\config\SYSTEM")
     
     match reg_hive.hive_type:
+
+        # Hanldes the SECURITY hive (located in the live registry in 'HKEY_LOCAL_MACHINE\SECURITY\')
+        case "security":
+            security_hive = SecurityHive(reg_hive)
+
+        # Hanldes the SOFTWARE hive (located in the live registry in 'HKEY_LOCAL_MACHINE\SOFTWARE\')
+        case "software":
+            software_hive = SoftwareHive(reg_hive)
+            print(software_hive.get_winlogon_shell())
 
         # Hanldes the SYSTEM hive (located in the live registry in 'HKEY_LOCAL_MACHINE\SYSTEM\')
         case "system":
@@ -21,14 +30,7 @@ def main():
 
             last_shutdown_time = system_hive.get_last_shutdown()
             print(last_shutdown_time)
-
-        # Hanldes the SOFTWARE hive (located in the live registry in 'HKEY_LOCAL_MACHINE\SOFTWARE\')
-        case "software":
-            software_hive = SoftwareHive(reg_hive)
-            print(software_hive.get_winlogon_shell())
-
-        case "security":
-            security_hive = SecurityHive(reg_hive)
+        
 
 if __name__ == "__main__":
     main()
