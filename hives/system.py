a8861e49255445cbe38f5de265d3930c0630cdb2
Elad Levi <58475124+JakePeralta7@users.noreply.github.com>
2024-10-15 17:35:00 +0300

    Update system.py

--- hives/system.py
+++ hives/system.py
@@ -1,5 +1,5 @@
 from regipy.registry import RegistryHive
-from regipy.utils import convert_wintime
+import datetime
 
 
 class SystemHive:
@@ -19,3 +19,17 @@ class SystemHive:
                 self.current_control_set = "ControlSet002"
             case _:
                 raise Exception("Couldn't find the current control set")
+
+    def get_computer_name(self):
+        return self.reg_hive.get_key(fr"\{self.current_control_set}\Control\ComputerName\ComputerName").get_value("ComputerName")
+
+    def get_last_shutdown(self):
+        shutdown_time_hex_bytes = self.reg_hive.get_key(fr"\{self.current_control_set}\Control\Windows").get_value("ShutdownTime")
+
+        # Convert the hex value to a decimal timestamp
+        shutdown_time_decimal = int.from_bytes(shutdown_time_hex_bytes, byteorder='little', signed=False) / 10**7
+        
+        # Convert the decimal timestamp to a datetime object
+        shutdown_time = datetime.datetime(1601, 1, 1) + datetime.timedelta(seconds=shutdown_time_decimal)
+    
+        return shutdown_time
