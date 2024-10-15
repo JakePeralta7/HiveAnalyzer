fa2a72727388a15197a87987f1279a50bd66312c
Elad Levi <58475124+JakePeralta7@users.noreply.github.com>
2024-10-15 17:35:57 +0300

    Create software.py

---
+++ hives/software.py
@@ -0,0 +1,10 @@
+from regipy.registry import RegistryHive
+from regipy.utils import convert_wintime
+
+
+class SoftwareHive():
+    def __init__(self, reg_hive):
+        self.reg_hive = reg_hive
+
+    def get_winlogon_shell(self):
+        return self.reg_hive.get_key(r"\Microsoft\Windows NT\CurrentVersion\Winlogon").get_value("Shell")
