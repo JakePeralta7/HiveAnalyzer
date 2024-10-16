94eb60bbe34d7063ac163060c795e372bd20ef57
Elad Levi <58475124+JakePeralta7@users.noreply.github.com>
2024-10-16 22:47:39 +0300

    Update software.py

--- hives/software.py
+++ hives/software.py
@@ -1,3 +1,5 @@
+
+# Internal Imports
 from hive import Hive
 
 
@@ -6,4 +8,33 @@ class SoftwareHive(Hive):
         super().__init__(reg_hive)
 
     def get_winlogon_shell(self):
-        return self.reg_hive.get_key(r"\Microsoft\Windows NT\CurrentVersion\Winlogon").get_value("Shell")
+        reg_key = r"\Microsoft\Windows NT\CurrentVersion\Winlogon"
+        reg_value_name = "Shell"
+        return self.get_value_data(reg_key, reg_value_name)
+
+    def get_operating_system(self):
+        reg_key = r"\Microsoft\Windows NT\CurrentVersion"
+        reg_value_name = "ProductName"
+        return self.get_value_data(reg_key, reg_value_name)
+
+    def detect_disabled_event_log(self):
+        reg_key = r"\Microsoft\Windows\CurrentVersion\WINEVT\Channels"
+        reg_value_name = "Enabled"
+
+        timestamp_description = "Last Time Key Modified"
+        category = ""
+        description = "Disabled Important Event Logs"
+
+        important_event_logs = [
+            "Microsoft-Windows-TaskScheduler/Operational",
+            "Microsoft-Windows-PowerShell/Operational"
+            ]
+
+        for event_log_name in important_event_logs:
+            current_reg_key = fr"{reg_key}\{event_log_name}"
+            event_log_status, last_modified = self.get_value_data(current_reg_key, reg_value_name)
+            if self.get_value_data(current_reg_key, reg_value_name) == 0:
+                self.output.file_evidence(timestamp=last_modified, timestamp_description=timestamp_description,
+                                  source=None, category=category, registry_path=None, description=description,
+                                  finding=event_log_name)
+            
