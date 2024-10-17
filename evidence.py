451909591665b53b84a7cfb1756ed4c2dcb5830a
Elad Levi <58475124+JakePeralta7@users.noreply.github.com>
2024-10-17 13:08:31 +0300

    Update evidence.py

--- evidence.py
+++ evidence.py
@@ -1,26 +1,23 @@
 class Evidence:
-    def __init__(self, timestamp, timestamp_description, source, category, registry_path, description, finding):
+    def __init__(self, timestamp, category, timestamp_description, description, finding, registry_path, source):
 
         # The timestamp that's relevant for the evidence
         self.timestamp = timestamp
 
+        # Categorize the evidence (for example: 'General Information')
+        self.category = category
+
         # Explains what the timestamp stands for (for example: 'First Execution')
         self.timestamp_description = timestamp_description
 
-        # The hive file path from which the evidence was extracted
-        self.source = source
-
-        # Categoriize the evidence (for example: 'General Information')
-        self.category = category
-
-        # The path of the registry from which the evidence was extracted
-        self.registry_path = registry_path
-
-        # Decsription of the finding (for example: 'Computer Name')
+        # Description of the finding (for example: 'Computer Name')
         self.description = description
 
         # The data itself (for example: the computer's name)
         self.finding = finding
 
-    def __str__(self):
-        return f"{self.timestamp}, {self.category}, {self.description}, {self.finding}"
+        # The path of the registry from which the evidence was extracted
+        self.registry_path = registry_path
+
+        # The hive file path from which the evidence was extracted
+        self.source = source
