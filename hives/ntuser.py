e436353fc84cb24ef9ccf9aca913ede9cd526645
Elad Levi <58475124+JakePeralta7@users.noreply.github.com>
2024-10-17 13:07:28 +0300

    Update ntuser.py

--- hives/ntuser.py
+++ hives/ntuser.py
@@ -1,19 +1,23 @@
 
 # External Imports
 from regipy import convert_wintime
+from regipy.exceptions import RegistryKeyNotFoundException
+from pathlib import Path
 
 # Internal Imports
 from hive import Hive
 
 
 class NTUserHive(Hive):
-    def __init__(self, reg_hive):
-        super().__init__(reg_hive)
+    def __init__(self, reg_hive_path, reg_hive, output):
+        super().__init__(reg_hive_path, reg_hive, output)
+        self.prefix = "HKEY_CURRENT_USER"
+
+        # The Username is always the name of the directory in which NTUSER.DAT is stored
+        self.username = self.get_username()
 
     def get_username(self):
-        reg_key = r"\Volatile Environment"
-        reg_value_name = "USERNAME"
-        self.username = self.get_value_data(reg_key, reg_value_name)
+        return Path(self.reg_hive_path).parent.parts[-1]
 
     def is_user_signed_in(self):
         reg_key = r"\Software\Microsoft\Windows\CurrentVersion\Explorer"
@@ -31,14 +35,27 @@ class NTUserHive(Hive):
         reg_value_name = "LastLogonTime"
 
         last_logon_time_hex_bytes = self.get_value_data(reg_key, reg_value_name)
-        last_logon_time = self.convert_filetime_timestamp(last_logon_time_hex_bytes)
+        last_logon_time = self.convert_hex_filetime(last_logon_time_hex_bytes)
     
         return last_logon_time
 
     def get_used_sysinternals(self):
         reg_key =r"\Software\Sysinternals"
 
-        for subkey in self.reg_hive.get_key(reg_key).iter_subkeys():
-            program_name = subkey.name
-            first_used = convert_wintime(subkey.header.last_modified)
-            print(f"{program_name} - {first_used}")
+        timestamp_description = "First Used"
+        category = "Evidence of Execution"
+        description = f"{self.username} accepted the EULA of a Sysinternals program"
+
+        try:
+            for subkey in self.reg_hive.get_key(reg_key).iter_subkeys():
+                program_name = subkey.name
+
+                registry_path = fr"{self.prefix}{reg_key}\{program_name}"
+
+                last_modified = self.convert_decimal_filetime(subkey.header.last_modified)
+
+                self.output.file_evidence(timestamp=last_modified, category=category,
+                                          timestamp_description=timestamp_description, description=description,
+                                          finding=program_name, registry_path=registry_path, source=self.reg_hive_path)
+        except RegistryKeyNotFoundException:
+            pass
