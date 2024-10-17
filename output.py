89525949babadd402b480bdc5999b7c7371000ed
Elad Levi <58475124+JakePeralta7@users.noreply.github.com>
2024-10-17 13:06:59 +0300

    Update output.py

--- output.py
+++ output.py
@@ -1,17 +1,30 @@
 
+# External Imports
+from csv import DictWriter
+
 # Internal Imports
 from evidence import Evidence
 
 
 class Output:
-    def __init__(self, output_format="csv"):
+    def __init__(self, output_format="csv", output_path="test.csv"):
         self.output_format = output_format
+        self.output_path = output_path
         self.evidence_list = []
 
-    def file_evidence(self, timestamp, timestamp_description, source, category, registry_path, description, finding):
-        evidence = Evidence(timestamp, timestamp_description, source, category, registry_path, description, finding)
+    def file_evidence(self, timestamp, category, timestamp_description, description, finding, registry_path, source):
+        evidence = Evidence(timestamp, category, timestamp_description, description, finding, registry_path, source)
         self.evidence_list.append(evidence)
     
-    def export(self, file_path=None):
-        for evidence in self.evidence_list:
-            print(str(evidence))
+    def export(self):
+        match self.output_format:
+            case "csv":
+                with open(self.output_path, 'w', newline='') as csvfile:
+                    fieldnames = ["timestamp", "category", "timestamp_description", "description", "finding", "registry_path",
+                                  "source"]
+                    writer = DictWriter(csvfile, fieldnames=fieldnames)
+                    writer.writeheader()
+                    for current_evidence in self.evidence_list:
+                        current_evidence_dict = current_evidence.__dict__
+                        writer.writerow(current_evidence_dict)
+        print(f"output was successfully exported to '{self.output_path}'")
