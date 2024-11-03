c0ba856d7e67d28b1bc001a4568dc59f21ee75f9
Elad Levi <58475124+JakePeralta7@users.noreply.github.com>
2024-11-03 16:57:47 +0200

    Update __init__.py

--- HiveAnalyzer/__init__.py
+++ HiveAnalyzer/__init__.py
@@ -17,29 +17,30 @@ from HiveAnalyzer.findings_config_handler import FindingsConfigHandler
 logger = logging.getLogger(__name__)
 
 
-def handle_registry_hive(reg_hive_path, reg_hive, output):
+def handle_registry_hive(reg_hive_path, reg_hive, output, fc_handler):
     match reg_hive.hive_type:
 
         # Handles the SECURITY hive (located in the live registry in 'HKEY_LOCAL_MACHINE\SECURITY\')
         case "security":
-            security_hive = SecurityHive(reg_hive_path, reg_hive, output)
+            current_findings_config = fc_handler.finding_configs[reg_hive.hive_type]
+            security_hive = SecurityHive(reg_hive_path, reg_hive, output, current_findings_config)
 
         # Handles the SOFTWARE hive (located in the live registry in 'HKEY_LOCAL_MACHINE\SOFTWARE\')
         case "software":
-            software_hive = SoftwareHive(reg_hive_path, reg_hive, output)
+            current_findings_config = fc_handler.finding_configs[reg_hive.hive_type]
+            software_hive = SoftwareHive(reg_hive_path, reg_hive, output, current_findings_config)
             software_hive.get_winlogon_shell()
-            software_hive.get_operating_system()
             software_hive.detect_disabled_event_log()
 
         # Handles the SYSTEM hive (located in the live registry in 'HKEY_LOCAL_MACHINE\SYSTEM\')
         case "system":
-            system_hive = SystemHive(reg_hive_path, reg_hive, output)
-            system_hive.get_computer_name()
-            system_hive.get_last_shutdown()
+            current_findings_config = fc_handler.finding_configs[reg_hive.hive_type]
+            system_hive = SystemHive(reg_hive_path, reg_hive, output, current_findings_config)
 
         # Handles the NTUSER.DAT hive (located in the live registry in 'HKEY_CURRENT_USER\' for the appropriate user)
         case "ntuser":
-            ntuser_hive = NTUserHive(reg_hive_path, reg_hive, output)
+            current_findings_config = fc_handler.finding_configs[reg_hive.hive_type]
+            ntuser_hive = NTUserHive(reg_hive_path, reg_hive, output, current_findings_config)
             ntuser_hive.get_used_sysinternals()
 
         case "usrclass":
@@ -62,19 +63,19 @@ def handle_registry_hive(reg_hive_path, reg_hive, output):
 
 
 def main():
-    a = FindingsConfigHandler()
+    fc_handler = FindingsConfigHandler()
 
     logging.basicConfig(filename='test.log', level=logging.INFO)
     
     output = Output(output_format="csv", output_path="zibi.csv")
 
-    artifacts_dir = r"C:\Users\flare\Documents\output"
+    artifacts_dir = r"D:\Tools\KAPE\output"
     for dir_path, dir_names, file_names in walk(artifacts_dir):
         for file_name in file_names:
             current_file_path = fr"{dir_path}\{file_name}"
             try:
                 reg_hive = RegistryHive(current_file_path)
-                output = handle_registry_hive(current_file_path, reg_hive, output)
+                output = handle_registry_hive(current_file_path, reg_hive, output, fc_handler)
             except ConstError:
                 continue
 
