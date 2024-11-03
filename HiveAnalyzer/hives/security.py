34a42d558bda805541de8a39e477128a48aee7fe
Elad Levi <58475124+JakePeralta7@users.noreply.github.com>
2024-11-03 16:55:53 +0200

    Update security.py

--- HiveAnalyzer/hives/security.py
+++ HiveAnalyzer/hives/security.py
@@ -10,5 +10,7 @@ logger = logging.getLogger(__name__)
 
 
 class SecurityHive(Hive):
-    def __init__(self, reg_hive_path, reg_hive, output):
-        super().__init__(reg_hive_path, reg_hive, output)
+    def __init__(self, reg_hive_path, reg_hive, output, findings_config):
+        super().__init__(reg_hive_path, reg_hive, output, findings_config)
+        self.prefix = r"HKEY_LOCAL_MACHINE\SECURITY"
+        self.get_configurable()
